0abe8248437759f1dd36510aaa38f2cbcfccc4be
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2021-10-28 20:23:14 +0600

    updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -15,8 +15,8 @@ const Banner = () => {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>Professionalism</h3>
+                        <p>We believe in professionalism and service you professionally.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -27,8 +27,8 @@ const Banner = () => {
                     />
 
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3>We are Punctual</h3>
+                        <p>We know the importance of your valuable times, so we deliver you punctually.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -39,8 +39,8 @@ const Banner = () => {
                     />
 
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3>keep Trust On Our Expertise</h3>
+                        <p>We repair and resolve with international level trained mechanics.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
